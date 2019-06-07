ff52021986e635c42fac3976415fc4957dc3c0ab
HenryMorocho <51182172+HenryMorocho@users.noreply.github.com>
2019-06-07 03:29:46 -0400

    Update main.go
    Added functions to perform different methods

--- main.go
+++ main.go
@@ -10,6 +10,12 @@ import (
 	"time"
 )
 
+const (
+	url1 = "https://api.packet.net/projects"
+	url2 = "https://api.packet.net/projects/ca73364c-6023-4935-9137-2132e73c20b4/devices"
+	url3 = "https://api.packet.net/devices"
+)
+
 type obj struct {
 	Projects []struct {
 		Id   string `json:"id"`
@@ -17,9 +23,10 @@ type obj struct {
 	}
 }
 
-type obj1 struct {
-	Id    string `json:"id"`
-	State string `json:"state"`
+type device struct {
+	Id       string `json:"id"`
+	State    string `json:"state"`
+	Hostname string `json: "hostname"`
 }
 
 func getProjects(body []byte) (*obj, error) {
@@ -31,55 +38,73 @@ func getProjects(body []byte) (*obj, error) {
 	return s, err
 }
 
-func getDeviceInfo(body []byte) (*obj1, error) {
-	var s = new(obj1)
+func getDeviceInfo(body []byte) (*device, error) {
+	var s = new(device)
 	err := json.Unmarshal(body, &s)
 	if err != nil {
 		fmt.Println("whoops:", err)
 	}
 	return s, err
 }
-
-func main() {
-	// URL's with specific endpoints
-	url1 := "https://api.packet.net/projects"
-	url2 := "https://api.packet.net/projects/ca73364c-6023-4935-9137-2132e73c20b4/devices"
-	url3 := "https://api.packet.net/devices"
-	fmt.Println("URL:>", url1)
-	//Requesting a GET, for recieving project detials
-	req, err := http.NewRequest("GET", url1, nil)
+// this function takes a string "GET" and string url and returns a response status   
+func GetRequestMethod(a, b string) string {
+	req, err := http.NewRequest(a, b, nil)
 	if err != nil {
 		panic(err)
 	}
-	//Authentication, Packet API requires an API token in the header of new request
 	req.Header.Set("X-Auth-Token", "wbrYPDxpE1y8bT95WknGyJgrwPdsteVw")
 	req.Header.Set("Accept", "application/json")
-
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
 		panic(err)
 	}
 	defer resp.Body.Close()
-	fmt.Println("response Status:", resp.Status)
+	return resp.Status
+}
+//Inputs: "GET" string and url string, Outputs: Device info
+func RetrieveDeviceInfo(a, b string) (string, string) {
+	req, err := http.NewRequest(a, b, nil)
+	if err != nil {
+		panic(err)
+	}
+	req.Header.Set("X-Auth-Token", "wbrYPDxpE1y8bT95WknGyJgrwPdsteVw")
+	req.Header.Set("Accept", "application/json")
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	Dev, err := getDeviceInfo(body)
+	if err != nil {
+		panic(err)
+	}
+	return Dev.Id, Dev.Hostname
 
-	fmt.Println("Creating Device")
-	//Creating a device with specific detials and proper JSON format
+}
+// This function creates a device and returns device info
+func PostRequestMethod(a, b string) (string, string) {
+
 	device := []byte(`{"facility": "ewr1","plan":"t1.small.x86","operating_system":"ubuntu_16_04"}`)
-	req, err = http.NewRequest("POST", url2, bytes.NewBuffer(device))
+	req, err := http.NewRequest(a, b, bytes.NewBuffer(device))
 	req.Header.Set("X-Auth-Token", "wbrYPDxpE1y8bT95WknGyJgrwPdsteVw")
 	req.Header.Set("Content-Type", "application/json")
 
 	if err != nil {
 		panic(err)
 	}
-	resp, err = client.Do(req)
+	client := &http.Client{}
+	resp, err := client.Do(req)
 	if err != nil {
 		panic(err)
 	}
 	defer resp.Body.Close()
-	body, err = ioutil.ReadAll(resp.Body)
+	body, err := ioutil.ReadAll(resp.Body)
 
 	if err != nil {
 		panic(err)
@@ -88,27 +113,39 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
-	fmt.Println("Device:\n Id:", r.Id, "\nSate:", r.State)
+	return r.Id, r.State
+}
+// Deletes an existing device
+func DeleteDevice(a, b, c string) string {
 	fmt.Println("Deleting Device...")
-	//Timer allows for Device Processing after initial creation of device
-	timer1 := time.NewTimer(45 * time.Second)
-	<-timer1.C
-	
-	//Extracted device Id to then concatenate with url3
-	together := []string{url3, r.Id}
-	req, err = http.NewRequest("DELETE", strings.Join(together, "/"), nil)
+	url := []string{b, c}
+	req, err := http.NewRequest(a, strings.Join(url, "/"), nil)
 	req.Header.Set("X-Auth-Token", "wbrYPDxpE1y8bT95WknGyJgrwPdsteVw")
 	req.Header.Set("Content-Type", "application/json")
 	if err != nil {
 		panic(err)
 	}
-	resp, err = client.Do(req)
+	client := &http.Client{}
+	resp, err := client.Do(req)
 	if err != nil {
 		panic(err)
 	}
 	defer resp.Body.Close()
-	fmt.Println("response Status:", resp.Status)
+	return resp.Status
+}
 
+func main() {
+	fmt.Println("URL:>", url1)
+	status := GetRequestMethod("GET", url1)
+	fmt.Println("Response Status:", status)
+	fmt.Println("Ready to create device")
+	deviceId, deviceStat := PostRequestMethod("POST", url2)
+	fmt.Println("device: [\n   { Id:", deviceId, "\n     Status:", deviceStat, "\n   }\n]")
+	fmt.Println("Datacenter is provisioning device.")
+	timer1 := time.NewTimer(45 * time.Second)
+	<-timer1.C
+	deleteDevice := DeleteDevice("DELETE", url3, deviceId)
+	fmt.Println("Response Satuts: ", deleteDevice)
 	fmt.Print("Device has been deleted\nTerminating Program.")
+
 }
